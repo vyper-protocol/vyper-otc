7ede98706c3659730de83e6ccf74d87f9fa6101c
dutch <giacomo@vyperprotocol.io>
2022-07-29 09:10:21 +0200

    [VOTC-4] env data pre-ix

--- programs/vyper-otc/src/lib.rs
+++ programs/vyper-otc/src/lib.rs
@@ -3,7 +3,6 @@ pub mod instructions;
 pub mod state;
 
 use anchor_lang::prelude::*;
-
 use instructions::*;
 
 declare_id!("QRd16aFfip7CEaXZUMQva4p9YYaQSog3ncEDTLoZPsP");
@@ -12,6 +11,7 @@ declare_id!("QRd16aFfip7CEaXZUMQva4p9YYaQSog3ncEDTLoZPsP");
 pub mod vyper_otc {
     use super::*;
 
+    #[access_control(pre_ix("initialize"))]
     pub fn initialize(
         ctx: Context<InitializeContext>,
         input_data: InitializeInputData,
@@ -19,6 +19,7 @@ pub mod vyper_otc {
         instructions::initialize::handler(ctx, input_data)
     }
 
+    #[access_control(pre_ix("deposit"))]
     pub fn deposit(
         ctx: Context<DepositContext>,
         input_data: DepositInputData,
@@ -26,15 +27,30 @@ pub mod vyper_otc {
         instructions::deposit::handler(ctx, input_data)
     }
 
+    #[access_control(pre_ix("settle"))]
     pub fn settle(
         ctx: Context<RedeemContext>,
     ) -> Result<()> {
         instructions::settle::handler(ctx)
     }
 
+    #[access_control(pre_ix("claim"))]
     pub fn claim(
         ctx: Context<ClaimContext>,
     ) -> Result<()> {
         instructions::claim::handler(ctx)
     }
+}
+
+
+fn pre_ix(_method_name:&str) -> Result<()> {
+    #[cfg(feature = "env-log")]
+    {
+        msg!("env data:");
+        msg!("+ pkg name: {:?}", env!("CARGO_PKG_NAME"));
+        msg!("+ pkg description: {:?}", env!("CARGO_PKG_DESCRIPTION"));
+        msg!("+ pkg version: {:?}", env!("CARGO_PKG_VERSION"));
+    }
+
+    Ok(())
 }
