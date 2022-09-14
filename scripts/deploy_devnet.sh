d330def2097f8f8b0b74a59bd2c546a2fb62d3bf
dutch <giacomo@vyperprotocol.io>
2022-09-14 14:32:25 +0300

    upgrade idl script

--- scripts/deploy_devnet.sh
+++ scripts/deploy_devnet.sh
@@ -9,3 +9,6 @@ anchor deploy -p vyper-otc --provider.cluster d --provider.wallet ~/Dev/VyperWal
 
 # solana-keygen recover -o ./ephemeral-kp.json prompt:// 
 # solana program deploy --buffer ./ephemeral-kp.json -u d --upgrade-authority ~/Dev/VyperWallets/vyper-program-authority/authority.json -k ~/Dev/VyperWallets/vyper-program-authority/authority.json ./target/deploy/vyper_otc.so
+
+# upgrade the idl
+anchor idl upgrade -f target/idl/vyper_otc.json 8aHSkExY28qCvg4gnTLU7y1Ev6HnpJ1NxuWb9XtEesVt --provider.cluster devnet --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
